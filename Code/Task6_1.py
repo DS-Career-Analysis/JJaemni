726e48a690aba2cf7f793e50d3f441826b6ffb2a
Jaemin Hong <hjm7392@mail.ulsan.ac.kr>
2023-08-12 15:39:46 +0900

    Data Merge and Null Check

--- Code/Task6_1.py
+++ Code/Task6_1.py
@@ -1,12 +1,12 @@
 import pandas as pd
 pd.set_option('display.max_rows', 10)
 pd.set_option('display.max_columns', None)
-path = 'C:\\VSC\\JJaemni\\CSV\\'
-Task3_path = 'C:\\VSC\\JJaemni\\CSV\\Task3\\'
-Task4_path = 'C:\\VSC\\JJaemni\\CSV\\Task4\\'
+path = 'C:/VSC/JJaemni/CSV/'
+Task3_path = 'C:/VSC/JJaemni/CSV/Task3/'
+Task4_path = 'C:/VSC/JJaemni/CSV/Task4/'
 
 # 원티드 데이터
-wanted = pd.read_csv(Task3_path + 'wanted.csv')
+wanted = pd.read_csv(Task3_path + 'wanted_dup.csv')
 ds_w = pd.read_csv(Task3_path + 'wanted_3_Requirement.csv')
 da_w = pd.read_csv(Task3_path + 'wanted_4_Preference.csv')
 de_w = pd.read_csv(Task3_path + 'wanted_5_StackTool.csv')
@@ -24,12 +24,8 @@ for i in range(0, wanted.shape[0]):
 loc_w = pd.DataFrame({'Location': L})
 wanted.Location = loc_w
 
-# 원티드 wanted.csv 업데이트
-wanted.to_csv(Task3_path + 'wanted.csv', index=False, encoding='utf-8-sig')
-wanted = pd.read_csv(Task3_path + 'wanted.csv')
-
 # 잡플래닛 데이터
-jobplanet = pd.read_csv(Task4_path + 'jobplanet.csv')
+jobplanet = pd.read_csv(Task4_path + 'jobplanet_dup.csv')
 ds_j = pd.read_csv(Task4_path + 'jobplanet_3_Requirement.csv')
 da_j = pd.read_csv(Task4_path + 'jobplanet_4_Preference.csv')
 de_j = pd.read_csv(Task4_path + 'jobplanet_5_StackTool.csv')
@@ -45,9 +41,9 @@ df.reset_index(drop=True, inplace=True)
 sorted_df = df.sort_values('label')
 sorted_df.reset_index(drop=True, inplace=True)
 
-sorted_df.to_csv('C:\\VSC\\JJaemni\\CSV\\wanted_jobplanet.csv', index=False, encoding='utf-8-sig')
+sorted_df.to_csv('C:/VSC/JJaemni/CSV/wanted_jobplanet.csv', index=False, encoding='utf-8-sig')
 
 # Null값 확인
-sorted_df = pd.read_csv('C:\\VSC\\JJaemni\\CSV\\wanted_jobplanet.csv')
+sorted_df = pd.read_csv('C:/VSC/JJaemni/CSV/wanted_jobplanet.csv')
 print(f'{sorted_df.shape = }')
 print(f'## Null ##\n{sorted_df.isnull().sum()}')
